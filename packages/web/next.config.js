1851c3d5f1d07e6757dd739ad153f5fe0e7b8c8b
Chase Adams <c@cadams.io>
2019-03-25 23:05:31 -0700

    build(lighthouse): added lighthouse auditing to CI

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,41 +1,62 @@
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins');
 
-const withTypescript = require("@zeit/next-typescript");
-const withCSS = require("@zeit/next-css");
-const withSass = require("@zeit/next-sass");
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const nextRuntimeDotenv = require("next-runtime-dotenv");
+const withTypescript = require('@zeit/next-typescript');
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const withOffline = require('next-offline');
+const nextRuntimeDotenv = require('next-runtime-dotenv');
 
 const withConfig = nextRuntimeDotenv({
-  public: ["API_URL"]
+  public: ['API_URL']
 });
 
 module.exports = withPlugins(
-  [[withTypescript], [withCSS], [withSass], [withBundleAnalyzer], [withConfig]],
+  [[withTypescript], [withCSS], [withSass], [withBundleAnalyzer], [withConfig], [withOffline]],
   {
-    target: "serverless",
-    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    target: 'serverless',
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
     bundleAnalyzerConfig: {
       server: {
-        analyzerMode: "static",
-        reportFilename: "../bundles/server.html"
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html'
       },
       browser: {
-        analyzerMode: "static",
-        reportFilename: "../bundles/client.html"
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
       }
+    },
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: 'networkFirst',
+          options: {
+            cacheName: 'https-calls',
+            networkTimeoutSeconds: 10,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
     },
     webpack(config) {
       config.module.rules.push({
         test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
         use: {
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
             limit: 8192,
-            publicPath: "/_next/static/",
-            outputPath: "static/",
-            name: "[name].[ext]"
+            publicPath: '/_next/static/',
+            outputPath: 'static/',
+            name: '[name].[ext]'
           }
         }
       });
--- packages/web/pages/about.tsx
+++ packages/web/pages/about.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Grid, Header, Image, Segment } from 'semantic-ui-react';
-import packageJson from '../../../package.json';
+
+const description =
+  'An opinionated universal web app + API starter kit to facilitate ' +
+  'rapid and scalable development using NextJS, FeathersJS, and MongoDB.';
 
 const AboutPage: React.FC<void> = () => (
   <Segment style={{ padding: '8em 0em' }} vertical>
@@ -10,7 +13,7 @@ const AboutPage: React.FC<void> = () => (
           <Header as="h3" style={{ fontSize: '2em' }}>
             About
           </Header>
-          <p>{packageJson.description}</p>
+          <p>{description}</p>
         </Grid.Column>
         <Grid.Column floated="right" width={4}>
           <Image rounded size="large" src="/static/accelerate-starter.png" />
